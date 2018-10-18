261849fee01ba644104696e5213a3d331120b481
Max-Ignis-Media <max.w.millard@gmail.com>
2018-10-18 20:33:40 +0100

    fifth commit

--- index.py
+++ index.py
@@ -9,11 +9,47 @@ def index():
 def lrseries():
         return render_template("lrseries.html")
 
+@app.route("/lrseriesEO")
+def lrseriesEO():
+        return render_template("lrseriesEO.html")
+
+@app.route("/lrseriesET")
+def lrseriesET():
+        return render_template("lrseriesET.html")
+
+@app.route("/lrseriesEH")
+def lrseriesEH():
+        return render_template("lrseriesEH.html")
+
 @app.route("/oneten")
 def oneten():
         return render_template("oneten.html")
 
+@app.route("/onetenEO")
+def onetenEO():
+        return render_template("onetenEO.html")
+
+@app.route("/onetenET")
+def onetenET():
+        return render_template("onetenET.html")
+
+@app.route("/onetenEH")
+def onetenEH():
+        return render_template("onetenEH.html")
+
 @app.route("/defender")
 def defender():
         return render_template("defender.html")
+
+@app.route("/defenderEO")
+def defenderEO():
+        return render_template("defenderEO.html")
+
+@app.route("/defenderET")
+def defenderET():
+        return render_template("defenderET.html")
+
+@app.route("/defenderEH")
+def defenderEH():
+        return render_template("defenderEH.html")
 
