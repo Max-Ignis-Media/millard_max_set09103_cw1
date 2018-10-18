e3c0f0db99f9eb1fdae40efc1fff33974027c694
Max-Ignis-Media <max.w.millard@gmail.com>
2018-10-18 17:18:03 +0100

    forth commit

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def index():
 	return render_template("index.html")
+
+@app.route("/lrseries")
+def lrseries():
+        return render_template("lrseries.html")
+
+@app.route("/oneten")
+def oneten():
+        return render_template("oneten.html")
+
+@app.route("/defender")
+def defender():
+        return render_template("defender.html")
+
